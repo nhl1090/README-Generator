edcc0cb7a86e47636ce9b7a6c468ec638163335d
nhl1090 <neilhdz24@gmail.com>
2024-07-20 13:13:00 -0600

    Added prompts for project description, installation instructions, usage information, contributing guidelines, and test instructions per the acceptance criteria.

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
+
 const questions = [
     {
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:',
     },
 ];
 

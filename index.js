7ef578196164c31c38ff0ae2a5953fdc9a6b32cd
nhl1090 <neilhdz24@gmail.com>
2024-07-20 17:44:14 -0600

    Added function to write README to file

--- index.js
+++ index.js
@@ -94,7 +94,9 @@ Email: ${answers.email}
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
 
 // TODO: Create a function to initialize app
 function init() {

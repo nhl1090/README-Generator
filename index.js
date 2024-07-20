676d12093f52291afce9df87e71d527a2086e37f
nhl1090 <neilhdz24@gmail.com>
2024-07-20 12:22:25 -0600

    Included package for the application, created a function to write the README file, and added a fucntion to initialize the app.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -7,7 +8,9 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log('Initialization...');
+}
 
 // Function call to initialize app
 init();

290317666446dc31bdd0074bd5e8aaeb69790de8
nhl1090 <neilhdz24@gmail.com>
2024-07-20 12:55:12 -0600

    Added prompt for project title.

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
